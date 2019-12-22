7bf994ea6088b864a3459d46765787cb3fcb9ffe
Gabriel Santos <Gabriel.developer.santos@gmail.com>
2019-12-22 09:33:30 -0300

    Implementando calculadora Rest

---
+++ RestWithASPNET/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("{firstNuber}/{secondNuber}")]
+        public IActionResult Sum(string firstNuber, string secondNuber)
+        {
+             if (IsNumeric(firstNuber) && IsNumeric(secondNuber))
+            {
+            var sum = ConvertToDecimal(firstNuber) + ConvertToDecimal(secondNuber);
+            //var sum = firstNumber + secondNuber;
+            return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private Decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
