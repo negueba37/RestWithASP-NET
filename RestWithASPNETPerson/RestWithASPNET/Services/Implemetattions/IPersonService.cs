ebe0effe5aeebd12c3780523d931b364dceb6937
Gabriel Santos <Gabriel.developer.santos@gmail.com>
2019-12-22 10:54:38 -0300

    Implementando os verbos

--- RestWithASPNETPerson/RestWithASPNET/Services/Implemetattions/IPersonService.cs
+++ RestWithASPNETPerson/RestWithASPNET/Services/Implemetattions/IPersonService.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using RestWithASPNET.Model;
 
 namespace RestWithASPNET.Services.Implemetattions
 {
     interface IPersonService
     {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
     }
 }
---
+++ RestWithASPNETPerson/RestWithASPNET/Services/Implemetattions/PersonServiceImpl.cs
@@ -0,0 +1,68 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestWithASPNET.Services.Implemetattions
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private volatile int conut;
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i= 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+
+                Id = IncrementAndGet(),
+                FirstName = "Person name "+i,
+                LasttName = "Person lastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref conut);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Gabriel",
+                LasttName = "Santos",
+                Address = "Cianorte",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
